0c94a29e556c70ed794bb40eecb330fb664d273f
Horus Lugo <horusgoul@gmail.com>
2020-10-27 19:49:47 +0100

    feat: add Game view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Game } from "./views/game";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Game />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
