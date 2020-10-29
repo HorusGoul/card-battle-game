12bf24fd79d1e6d11102c4206e1a243bb59359da
Horus Lugo <horusgoul@gmail.com>
2020-10-29 21:05:05 +0100

    feat: add basic host controls

--- src/components/host-controls/HostControls.tsx
+++ src/components/host-controls/HostControls.tsx
@@ -1,11 +1,59 @@
 import React from "react";
+import styles from "./HostControls.module.scss";
 import { useGame } from "../../context/game-state";
 import { GameHost } from "../../context/game-state/Game";
+import classnames from "classnames";
 
 function HostControls() {
-  const { state } = useGame<GameHost>();
+  const { state, game } = useGame<GameHost>();
 
-  return <div>{state.status === "waiting" && <button>Start game</button>}</div>;
+  const canStartGame = state.status === "waiting" && state.players.length > 1;
+  const inviteUrl = window.location.href;
+
+  function copyInviteUrl() {
+    if ("share" in navigator) {
+      navigator.share({
+        url: inviteUrl,
+        title: `Join me in Card Battle Game!`,
+      });
+    } else {
+      // @ts-ignore
+      navigator.clipboard.writeText(inviteUrl);
+      alert("URL Copied!");
+    }
+  }
+
+  if (
+    state.status === "connecting" ||
+    state.status === "cannot-join" ||
+    state.status === "finished"
+  ) {
+    return null;
+  }
+
+  return (
+    <div className={styles.hostControls}>
+      <span className={styles.tag}>Host Controls</span>
+
+      <div
+        className={classnames(styles.status, {
+          [styles.online]: game.online,
+        })}
+        aria-label={game.online ? `Online: ${state.players.length}` : `Offline`}
+      >
+        <div className={styles.ball} />
+        {state.players.length}
+      </div>
+
+      <button className={styles.inviteUrlCopyBtn} onClick={copyInviteUrl}>
+        Copy Invite URL
+      </button>
+
+      <button className={styles.startGameBtn} disabled={!canStartGame}>
+        {canStartGame ? "Start" : "Waiting"}
+      </button>
+    </div>
+  );
 }
 
 export default HostControls;
